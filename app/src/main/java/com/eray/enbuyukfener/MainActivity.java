a0790c8c0173bda615b59b5407c91a39cac8bfbd
eray-onur <eo.developer.0@gmail.com>
2020-03-01 13:48:06 +0300

    Uygulamanin Boilerplate'i kuruldu.

--- app/src/main/java/com/eray/enbuyukfener/MainActivity.java
+++ app/src/main/java/com/eray/enbuyukfener/MainActivity.java
@@ -2,13 +2,24 @@ package com.eray.enbuyukfener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnAllah;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAllah = findViewById(R.id.btnAllah);
+        btnAllah.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SecondaryActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        });
     }
+
+
 }
