12113a7bc4519a65813337a3c8dcb1dd4d3564cb
eray-onur <eo.developer.0@gmail.com>
2020-03-09 01:40:32 +0300

    Final version of the starting boilerplate.

---
+++ app/src/main/java/com/eray/enbuyukfener/Activities/SecondaryActivity.java
@@ -0,0 +1,16 @@
+package com.eray.enbuyukfener.Activities;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.eray.enbuyukfener.R;
+
+public class SecondaryActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
--- app/src/main/java/com/eray/enbuyukfener/MainActivity.java
+++ app/src/main/java/com/eray/enbuyukfener/MainActivity.java
@@ -1,25 +1,63 @@
 package com.eray.enbuyukfener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.eray.enbuyukfener.Fragments.BasketFragment;
+import com.eray.enbuyukfener.Fragments.HomeFragment;
+import com.eray.enbuyukfener.Fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-    private Button btnAllah;
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private TextView toolbarTitle;
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnAllah = findViewById(R.id.btnAllah);
-        btnAllah.setOnClickListener(view -> {
-            Intent intent = new Intent(this, SecondaryActivity.class);
-            startActivity(intent);
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-        });
+        toolbarTitle = findViewById(R.id.title);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavbar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        loadFragment(new HomeFragment());
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch(menuItem.getItemId()) {
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                toolbarTitle.setText(R.string.tab_home);
+                break;
+            case R.id.nav_profil:
+                fragment = new ProfileFragment();
+                toolbarTitle.setText(R.string.tab_profile);
+                break;
+            case R.id.nav_sepet:
+                fragment = new BasketFragment();
+                toolbarTitle.setText(R.string.tab_bucket);
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
 }
